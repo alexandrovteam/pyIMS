82f4bcd57a9b2c7c3b082e4a2543f3132eadba1f
Artem Tarasov <artem.tarasov@embl.de>
2016-09-09 15:56:24 +0200

    avoid scikit-image import by inlining needed code

--- pyImagingMSpec/convert/bruker_sqlite.py
+++ pyImagingMSpec/convert/bruker_sqlite.py
@@ -1,9 +1,22 @@
 import sqlite3
 import numpy as np
 import argparse
-from skimage.filters import threshold_otsu
-
 from pyimzml.ImzMLWriter import ImzMLWriter
+
+def threshold_otsu(image, nbins=256):
+    # Stripped-down function from scikit-image to avoid the heavy dependency.
+    assert image.min() != image.max()
+    hist, bin_edges = np.histogram(image.flat, bins=nbins)
+    bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2.
+    hist = hist.astype(float)
+    weight1 = np.cumsum(hist)
+    weight2 = np.cumsum(hist[::-1])[::-1]
+    mean1 = np.cumsum(hist * bin_centers) / weight1
+    mean2 = (np.cumsum((hist * bin_centers)[::-1]) / weight2[::-1])[::-1]
+    variance12 = weight1[:-1] * weight2[1:] * (mean1[:-1] - mean2[1:]) ** 2
+    idx = np.argmax(variance12)
+    threshold = bin_centers[:-1][idx]
+    return threshold
 
 class Spectrum(object):
     def __init__(self, row):
